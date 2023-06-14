768cd1788954f59cc59f0846f5b60b4140c8ff8d
soretaka <yatesthap@gmail.com>
2023-06-14 16:56:20 +0700

    feat: recommend park

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -16,7 +16,6 @@ export default function Home() {
   const [defaultCenter, setDefaultCenter] = useState([-7.284691674524294, 112.793789836901]);
   const [selectedArea, setSelectedArea] = useState();
   const ref = database.ref('area');
-
   useEffect(() => {
     ref.on('value', (snapshot) => {
       const data = snapshot.val();
@@ -34,6 +33,50 @@ export default function Home() {
       }
     });
   }, [selectedArea]);
+
+  function deg2rad(deg) {
+    return deg * (Math.PI / 180);
+  }
+  function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+    const R = 6371; // Radius of the earth in km
+    const dLat = deg2rad(lat2 - lat1); // deg2rad below
+    const dLon = deg2rad(lon2 - lon1);
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+      + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2))
+      * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c; // Distance in km
+    return d;
+  }
+
+  function getRecArea({ name }) {
+    let recommendedArea = name;
+    let dist = 0;
+    let lat = 0;
+    let lon = 0;
+    const availableArea = [];
+    area.forEach((park) => {
+      if (park.current_slot < park.max_slot && park.status) {
+        availableArea.push(park);
+      }
+      if (park.name === name) {
+        lat = park.latitude;
+        lon = park.longitude;
+      }
+    });
+    availableArea.forEach((park) => {
+      const nD = getDistanceFromLatLonInKm(park?.latitude, park?.longitude, lat, lon);
+      if (dist === 0) {
+        dist = nD;
+        recommendedArea = park;
+      }
+      if (dist > nD) {
+        dist = nD;
+        recommendedArea = park;
+      }
+    });
+    return recommendedArea.name;
+  }
 
   const handleSelectedArea = (event) => {
     event.preventDefault();
@@ -82,6 +125,15 @@ export default function Home() {
                       Slot:
                       {' '}
                       {park.current_slot}
+                      <br />
+                      Status:
+                      {park.status ? park.current_slot === park.max_slot ? 'penuh' : 'kosong' : 'tutup'}
+                      <br />
+                      Recommended Area:
+                      {' '}
+                      {((park.current_slot === park.max_slot) || (!park.status)) ? getRecArea({
+                        name: park.name,
+                      }) : 'parkir disini aja'}
                     </Popup>
                   </Marker>
                 ))}
